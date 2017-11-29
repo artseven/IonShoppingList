276a4fe4741ae93f74538a736b1325074c21b14e
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-29 12:26:14 -0500

    outputting list of items from db

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
 import { IonicPage } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../../models/item/item.model';
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,8 +11,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  shoppingList$: Observable<Item[]>
+  constructor(
+    public navCtrl: NavController,
+    private shopping: ShoppingListService
+  ) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList() //returns DB LIST
+      .snapshotChanges() // Key and value pairs
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key, ...c.payload.val()
+          }))
+        }
+      )
+    }
 
 }
